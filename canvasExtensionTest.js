8cb15c46be216f280a3efa69b537636cf0404a5b
Ted Goddard <ted.goddard@robotsandpencils.com>
2015-05-29 10:05:08 -0600

    added visualblocks menu item

--- canvasExtensionTest.js
+++ canvasExtensionTest.js
@@ -181,7 +181,7 @@ if (window.tinyMCE && nearPage("/editor-extension-test")) {
       arguments[0].content_css = ["/stylesheets_compiled/legacy_normal_contrast/bundles/what_gets_loaded_inside_the_tinymce_editor.css", "https://tedgoddard.github.io/canvas-edit/canvasExtensionTest.css"];
 //      arguments[0].plugins += ",visualblocks";
       arguments[0].external_plugins["visualblocks"] = "https://tedgoddard.github.io/canvas-edit/visualblocks/plugin.min.js";
-      arguments[0].toolbar[1] += "visualblocks";
+      arguments[0].toolbar[1] += ",visualblocks";
       oldInit.apply(this, arguments);
     }
 
